96e9c483f80f5d2c8af5816dcaa5498c3abef1e3
Inkan <inkandev@gmail.com>
2018-10-30 04:19:46 +0100

    Format event to include datetime

--- log.py
+++ log.py
@@ -1,10 +1,15 @@
+import datetime
+
+
 class Log:
 
     def __init__(self):
         pass
 
     def event(self, event: str):
-        self.__display(event)
+        time = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+        formatted_event = time + " " + event
+        self.__display(formatted_event)
 
     def __display(self, event: str):
         print(event)
