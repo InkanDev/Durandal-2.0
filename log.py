f6a4076bde7f02f1d74167aa4fc90d7e922a62a0
Inkan <inkandev@gmail.com>
2018-11-01 23:11:32 +0100

    Make methods statics

--- log.py
+++ log.py
@@ -3,19 +3,18 @@ import datetime
 
 class Log:
 
-    def __init__(self):
-        pass
-
-    def event(self, event: str):
+    @staticmethod
+    def event(event: str):
         time = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
         formatted_event = time + " " + event
-        self.__display(formatted_event)
+        Log.__display(formatted_event)
 
-    def __display(self, event: str):
+    @staticmethod
+    def __display(event: str):
         print(event)
 
-    def __write_in_file(self, event: str):
+    @staticmethod
+    def __write_in_file(event: str):
         log_file = open("logs.txt", "a+")
         log_file.write(event + "\n")
         log_file.close()
-
