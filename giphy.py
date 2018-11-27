2a8df95e8144bdf7ad54561cef685b48a4ce970c
Inkan <inkandev@gmail.com>
2018-11-27 10:32:07 +0100

    Add search method
    The static search method returns a list of gifs corresponding to the given keywords

--- giphy.py
+++ giphy.py
@@ -7,3 +7,8 @@ class Giphy:
 
     api_instance = giphy_client.DefaultApi()
     api_key = open("giphy_key.txt", "r").read()
+
+    @staticmethod
+    def search(keywords):
+        api_response = Giphy.api_instance.gifs_search_get(Giphy.api_key, keywords)
+        return api_response.data
