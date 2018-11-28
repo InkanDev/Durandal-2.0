65937c467007a7e9b6b700d7436711f588e352db
Inkan <inkandev@gmail.com>
2018-11-28 05:11:19 +0100

    Add get_random_gif method

--- giphy.py
+++ giphy.py
@@ -1,6 +1,7 @@
 # Wrapper for the Giphy API
 
 import giphy_client
+from random import randint
 
 
 class Giphy:
@@ -12,6 +13,11 @@ class Giphy:
     def search(keywords):
         api_response = Giphy.api_instance.gifs_search_get(Giphy.api_key, keywords, limit=100)
         return api_response.data
+
+    @staticmethod
+    def get_random_gif(gif_list):
+        random = randint(0, len(gif_list)-1)
+        return gif_list[random]
 
     @staticmethod
     def get_url(gif):
